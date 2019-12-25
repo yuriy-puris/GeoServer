ef50ed3e1573db60e8501c3e6b96ee21bcb0c1f1
Yuriy Puris <donovanyuriy@gmail.com>
2019-12-25 13:47:52 +0200

    first

--- app/settings.py
+++ app/settings.py
@@ -1,7 +1,6 @@
 import os
 import dj_database_url
-# import django_heroku
-# django_heroku.settings(locals())
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -136,3 +135,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/' 
+
+import django_heroku
+django_heroku.settings(locals())
