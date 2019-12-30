3c7759e24a9f78e933f4bb174c3fa0a8f5220566
Yuriy Puris <donovanyuriy@gmail.com>
2019-12-30 16:42:05 +0200

    add module

--- app/settings.py
+++ app/settings.py
@@ -82,6 +82,10 @@ DATABASES = {
     }
 }
 
+DATABASE_URL = os.environ.get('DATABASE_URL')
+db_from_env = dj_database_url.config(default=DATABASE_URL, conn_max_age=500, ssl_require=True)
+DATABASES['default'].update(db_from_env)
+
 # db_from_env = dj_database_url.config()
 # DATABASES['default'].update(db_from_env)
 
