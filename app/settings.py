26d3660a15c71be6225db46d70381ad86de7aaee
Yuriy Puris <donovanyuriy@gmail.com>
2019-12-30 17:48:02 +0200

    add

--- app/settings.py
+++ app/settings.py
@@ -2,6 +2,8 @@ import os
 import django_heroku
 import dotenv
 import dj_database_url
+
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
