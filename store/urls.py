fad6133972bd7b250e6c75ee4e5f7160ab971cf0
Yuriy Puris <donovanyuriy@gmail.com>
2019-12-30 20:50:27 +0200

    migrations

--- store/urls.py
+++ store/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import AddressView, CoordinatesView, ParseLocationView
 
 urlpatterns = [
-  path('/address', AddressView.as_view(), name='address'),
-  path('/coordinates', CoordinatesView.as_view(), name='coordinate'),
-  path('/post_location', ParseLocationView.as_view(), name='post_location'),
+  path('address', AddressView.as_view(), name='address'),
+  path('coordinates', CoordinatesView.as_view(), name='coordinate'),
+  path('post_location', ParseLocationView.as_view(), name='post_location'),
 ]
