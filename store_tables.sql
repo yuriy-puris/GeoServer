aa39f506348876bb4d594f9b01fee0281689487f
Yuriy Puris <ypuris@itera-research.com>
2020-02-12 12:32:23 +0200

    upd

--- store_tables.sql
+++ store_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE shop (
-  id INT,
+  id INT IDENTITY,
   name VARCHAR(255) NOT NULL,
   url VARCHAR(255) NOT NULL,
   search_request VARCHAR(255) NOT NULL,
@@ -8,7 +8,7 @@ CREATE TABLE shop (
 );
 
 CREATE TABLE address (
-  id INT,
+  id INT IDENTITY,
   shop_address VARCHAR(255) NOT NULL,
   shop_id INT NOT NULL,
 
@@ -17,7 +17,7 @@ CREATE TABLE address (
 );
 
 CREATE TABLE coordinates (
-  id INT,
+  id INT IDENTITY,
   lat FLOAT,
   lng FLOAT,
   address_id INT NOT NULL,
