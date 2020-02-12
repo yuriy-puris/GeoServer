b0d4855722bc348eccb87f9f9e52beeb6a945991
Yuriy Puris <ypuris@itera-research.com>
2020-02-12 12:16:41 +0200

    fx syntax sql files

--- store_tables.sql
+++ store_tables.sql
@@ -1,17 +1,17 @@
 CREATE TABLE shop (
-  [name] VARCHAR(255) NOT NULL,
-  [url] VARCHAR(255) NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  url VARCHAR(255) NOT NULL,
   search_request VARCHAR(255) NOT NULL,
 
   PRIMARY KEY (id)
 );
 
-CREATE TABLE [address] (
+CREATE TABLE address (
   shop_address VARCHAR(255) NOT NULL,
   shop_id INT NOT NULL,
 
   PRIMARY KEY (id),
-  FOREIGN KEY (shop_id) REFERENCES [shop](id)  
+  FOREIGN KEY (shop_id) REFERENCES shop(id)  
 );
 
 CREATE TABLE coordinates (
@@ -20,5 +20,5 @@ CREATE TABLE coordinates (
   address_id INT NOT NULL,
 
   PRIMARY KEY (id),
-  FOREIGN KEY (address_id) REFERENCES [address](id)
+  FOREIGN KEY (address_id) REFERENCES address(id)
 );
