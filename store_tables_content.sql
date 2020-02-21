a9997ea5e751d0ee3a76a306cfbf978ec10c3057
Yuriy Puris <ypuris@itera-research.com>
2020-02-21 16:52:49 +0200

    store shop content

--- store_tables_content.sql
+++ store_tables_content.sql
@@ -1,4 +1,10 @@
 INSERT INTO store_shop  ( name, url, search_request )
-VALUES ( 'test_name', 'test_url', 'test_search_request' ),
-       ( 'test_name1', 'test_url1', 'test_search_request1' ),
-       ( 'test_name2', 'test_url2', 'test_search_request2' );
+VALUES ( 'https://www.citrus.ua/', 'citrus', 'https://www.citrus.ua/search?query=' ),
+       ( 'https://www.moyo.ua/', 'moyo', 'https://www.moyo.ua/search/new/?q=' ),
+       ( 'https://eldorado.ua/', 'eldorado', 'https://eldorado.ua/search?q=' ),
+       ( 'https://allo.ua/', 'allo', 'https://allo.ua/ua/catalogsearch/result/?q=' ),
+       ( 'https://www.foxtrot.com.ua/', 'foxtrot', 'https://www.foxtrot.com.ua/ru/search?query=' ),
+       ( 'https://comfy.ua/', 'comfy', 'https://comfy.ua/catalogsearch/result?strategy=simple,zero_queries&search_provider=anyquery&q=' ),
+       ( 'https://pcshop.ua/', 'pcshop', 'https://pcshop.ua/index.php?route=product%2Fsearch&search=' ),
+       ( 'https://elmir.ua/', 'elmir', 'https://elmir.ua/?q=' ),
+       ( 'http://tehnostok.net/', 'tehnostock', 'http://tehnostok.net/index.php?controller=search&orderby=position&orderway=desc&search_query=' );
