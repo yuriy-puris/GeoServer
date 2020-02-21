5e3dcbe5118b834a4da37f8a178321265c9ab573
Yuriy Puris <ypuris@itera-research.com>
2020-02-21 17:16:56 +0200

    upd tbl

--- store/models.py
+++ store/models.py
@@ -11,13 +11,13 @@ class Shop(models.Model):
 
 class Address(models.Model):
     address = models.CharField(max_length=200)
-    shop_id = models.ForeignKey(Shop, on_delete=models.CASCADE)
+    shop_id = models.OneToOneField(Shop, on_delete=models.DO_NOTHING)
 
     def __str__(self):
         return "%s %s" % (self.address, self.shop_id)
 
 class Coordinates(models.Model):
-    adress_id = models.ForeignKey(Address, on_delete=models.CASCADE)
+    adress_id = models.OneToOneField(Address, on_delete=models.DO_NOTHING)
     lat = models.FloatField()
     lng = models.FloatField()
 
