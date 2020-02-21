3cdd6958aea4d9c17edf3283fdbd9636520a3cd5
Yuriy Puris <ypuris@itera-research.com>
2020-02-21 16:09:22 +0200

    test table

--- store_tables_content.sql
+++ store_tables_content.sql
@@ -1,4 +1,4 @@
-INSERT INTO shop ( name, url, search_request )
+INSERT INTO store_shop  ( name, url, search_request )
 VALUES ( 'test_name', 'test_url', 'test_search_request' ),
        ( 'test_name1', 'test_url1', 'test_search_request1' ),
        ( 'test_name2', 'test_url2', 'test_search_request2' );
